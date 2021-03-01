949775484b105d23e96f997b4cd86fd1f760d61d
Aleksandra Maratkanova <ola.maratkanova@gmail.com>
2021-03-01 20:46:34 +0400

    add

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let a = String(n);
+  let r = a.split('')
+    if(r[0] == '-') {r.splice(0,1)}
+    r.reverse()
+    if(r[0]=='0'){r.splice(0,1)}
+    a = r.join('')
+    return a
 }
